b717287441f37654a9099b4aa044327c1df8d8bf
sergio15dev <sergio15dev@gmail.com>
2022-12-21 10:55:47 -0500

    deploy port

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     $DB_USER = $_ENV["DB_USER"];
     $DB_PASSWORD = $_ENV["DB_PASSWORD"];
     $DB_NAME = $_ENV["DB_NAME"];
-    $DB_PORT = $_ENV["DB_PORT"];
+    // $DB_PORT = $_ENV["DB_PORT"];
 
     //PARTE DE COOKIES para el color de Fondo:-----
     if (isset($_COOKIE['colorFondo'])) {
